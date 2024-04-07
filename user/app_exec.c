168096885227e5f09f84321f3b74ccc070dbbba6
leaveboring <2712477103@qq.com>
2024-04-07 19:55:15 +0800

    debug l4c2

--- user/app_exec.c
+++ user/app_exec.c
@@ -1,9 +1,10 @@
-#include "user_lib.h"
+ #include "user_lib.h"
 #include "util/types.h"
 
 int main(int argc, char *argv[]) {
-  printu("\n======== exec /bin/app_ls in app_exec ========\n");
-  int ret = exec("/bin/app_ls");
+  printu("\n======== exec /bin/app_read in app_exec ========\n");
+  printu("call exec in main\n");
+  int ret = exec("/bin/app_read");
   if (ret == -1)
     printu("exec failed!\n");
 
